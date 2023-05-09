ecff6c78368accf64a7c8c0f2dae9c0a34f96446
Alex Okhtov <hasanohtov@yandex.ru>
2023-05-09 20:05:47 +0300

    added animations view and buttons

---
+++ MyMovie/Animations/UIViewAnimations.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewAnimations.swift
+//  MyMovie
+//
+//  Created by Alex Okhtov on 09.05.2023.
+//
+
+import UIKit
+
+enum ViewAnimationFactory {
+    static func makeEaseInAnimation(duration: TimeInterval, delay: TimeInterval, action: @escaping() -> Void) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+            action()
+        })
+    }
+    static func makeEaseInOutAnimation(duration: TimeInterval, delay: TimeInterval, action: @escaping() -> Void) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseInOut, animations: {
+            action()
+        })
+    }
+    static func makeEaseOutAnimation(duration: TimeInterval, delay: TimeInterval, action: @escaping() -> Void) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseOut, animations: {
+            action()
+        })
+    }
+    
+    static func makeSimpleAnimation(duration: TimeInterval, action: @escaping() -> Void){
+        UIView.animate(withDuration: duration) {
+            action()
+        }
+    }
+}
